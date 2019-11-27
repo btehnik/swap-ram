ae9d79d061249ca0a1676b430426c08172763a64
btehnik <craazy9@gmail.com>
2019-11-27 17:16:37 +0200

    renew swap.sh

--- swap.sh
+++ swap.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 SUM=0
+RES=0
+
 for DIR in `find /proc/ -maxdepth 1 -type d | egrep "^/proc/[0-9]"` ; do
         PID=`echo $DIR | cut -d / -f 3`
-        PROGNAME=`ps -p $PID -o comm --no-headers`
+        PROGNAME=`grep Name $DIR/status 2>/dev/null | awk '{print $2}'`
         for SWAP in `grep Swap $DIR/smaps 2>/dev/null| awk '{ print $2 }'`
         do
                 let SUM=$SUM+$SWAP/1024
         done
-        echo "PID: $PID - Swap used: $SUM MB - ($PROGNAME )"
+        echo " PID: $PID - Swap used: $SUM MB - ($PROGNAME )"
         SUM=0
-done
 
-########################################################################
-#									#
-# To run the script use command:					#
-# bash swap.sh | egrep -v "Swap used: 0" | sort -rn -k 6 | head -n 10	#
-#									#
-########################################################################
+done > $RES
+
+egrep -v "Swap used: 0" $RES | sort -rn -k 6 | head -n 10 | cat -n
