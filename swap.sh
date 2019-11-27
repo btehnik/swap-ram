a28941a2602f06a1bd07906cbe1adaea3f33f23d
btehnik <54862740+btehnik@users.noreply.github.com>
2019-11-27 12:46:13 +0200

    Update swap.sh
    Some little change

--- swap.sh
+++ swap.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 SUM=0
-OVERALL=0
-# get process list
 for DIR in `find /proc/ -maxdepth 1 -type d | egrep "^/proc/[0-9]"` ; do
         PID=`echo $DIR | cut -d / -f 3`
         PROGNAME=`ps -p $PID -o comm --no-headers`
